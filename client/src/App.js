782a9f62ff303ba8ca082a57a97e14f0f3de5a18
ancs214 <ancs214@gmail.com>
2022-07-16 08:01:00 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to ProShop</h1>
+      <a target="_blank" href="https://icons8.com/icon/13010/shop">Shop icon by Icons8</a>
+    </>
+  );
+}
+
+export default App;
