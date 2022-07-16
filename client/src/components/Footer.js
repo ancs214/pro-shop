748ad9ca6e1daaa9b07c5ff2234129f42b77d899
ancs214 <ancs214@gmail.com>
2022-07-16 11:24:46 -0400

    set up home page and product component rendered on home page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 
 
 const Footer = () => {
@@ -8,7 +8,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        <a target="_blank" href="https://icons8.com/icon/13010/shop">Shop icon by Icons8</a>
+                        <a target="_blank" rel='noreferrer' href="https://icons8.com/icon/13010/shop">Shop icon by Icons8</a>
                         <br />
                         Copyright &copy; ProShop
                     </Col>
